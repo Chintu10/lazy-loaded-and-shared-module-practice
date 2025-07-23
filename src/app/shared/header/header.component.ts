3a064ff9824f5c20a7fbba639705f91e9cfc0f13
chintu10 <chandrakant.sarle.main@gmail.com>
2025-07-23 10:19:03 +0530

    interpolation and property binding

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public title: string = '';
+  public image: string = '';
+  public hint: string = '';
+  public isMobile: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.checkMobile();
+    this.title = 'Header component from interpolation';
+    this.image = 'https://angular.io/assets/images/logos/angular/angular.png';
+    this.hint = 'This is a hint header component';
   }
 
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkMobile();
+  }
+
+  private checkMobile() {
+    this.isMobile = window.innerWidth < 768;
+  }
 }
