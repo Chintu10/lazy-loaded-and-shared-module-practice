fc8ba71f06a30c9858ec33add6dd16fe39f13f66
chintu10 <chandrakant.sarle.main@gmail.com>
2025-08-11 10:40:10 +0530

    2 way data binding and it'scomplex  alternative

--- src/app/admin/settings/settings.component.ts
+++ src/app/admin/settings/settings.component.ts
@@ -6,13 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-
    public name: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
     this.name='';
+  }
+
+  onInputChange(event: Event) {
+    const value =  (event?.target as HTMLInputElement).value;
+    this.name = value;
   }
 
 }
